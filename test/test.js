5b19e441716dc3ef19de0e58519cbc7a79acddcf
Tom Kirkpatrick <tom@systemseed.com>
2016-01-28 08:25:41 +0100

    Use supertest-as-promised in test suite

--- test/test.js
+++ test/test.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const path = require('path');
-const SIMPLE_APP = path.join(__dirname, 'fixtures', 'simple-app');
-const app = require(path.join(SIMPLE_APP, 'server/server.js'));
-const request = require('supertest');
+const request = require('supertest-as-promised');
 const chai = require('chai');
 const expect = chai.expect;
 const assert = require('assert');
@@ -11,7 +9,8 @@ const assert = require('assert');
 chai.use(require('sinon-chai'));
 require('mocha-sinon');
 
-global.Promise = require('bluebird');
+const SIMPLE_APP = path.join(__dirname, 'fixtures', 'simple-app');
+const app = require(path.join(SIMPLE_APP, 'server/server.js'));
 
 function json(verb, url) {
   return request(app)[verb](url)
@@ -20,52 +19,54 @@ function json(verb, url) {
     .expect('Content-Type', /json/);
 }
 
-describe('REST API request', function() {
+describe('REST API request', () => {
   // before(function(done) {
   //   require('./start-server');
   //   done();
   // });
 
-  after(function(done) {
+  after(function() {
     app.removeAllListeners('started');
     app.removeAllListeners('loaded');
-    expect(1);
-    done();
   });
 
-  it('should not allow access without access token', function(done) {
-    json('get', '/api/stuff')
-      .expect(401, done);
+  it('should not allow access without access token', function() {
+    return json('get', '/api/stuff')
+      .expect(401);
   });
 
-  it('should login as a team member and get a list of stuff', function(done) {
-    json('post', '/api/users/login')
+  it('should login as a team member and get a list of stuff', function() {
+    let accessToken = null;
+
+    return json('post', '/api/users/login')
       .send({ username: 'programAdminA', password: 'password' })
-      .expect(200, (err, res) => {
-        assert(!err);
+      .expect(200)
+      .then(res => {
         assert(typeof res.body === 'object');
         assert(res.body.id, 'must have an access token');
         assert.equal(res.body.userId, 'programAdminA');
-        const accessToken = res.body.id;
+        accessToken = res.body.id;
+      })
+      .then(() => json('get', `/api/stuff/1?access_token=${accessToken}`).expect(200))
+      .then(res => {
+        const stuff = res.body;
 
-        console.log(accessToken);
-        json('get', `/api/stuff/1?access_token=${accessToken}`)
-          .expect(200, (err, res) => {
-            // expect(err).to.not.exist;
-            const stuff = res.body;
-
-            assert(typeof stuff === 'object');
-            assert.equal(stuff.name, 'Widget 1');
-          });
-        done();
+        assert(typeof stuff === 'object');
+        assert.equal(stuff.name, 'Widget 1');
       });
   });
+
+  it('should not allow access without access tokenshoul', function() {
+    return json('get', '/api/stuff')
+      .expect(401);
+  });
+
 });
 
-describe('Unexpected Usage', function() {
-  it('should not crash the server when posting a bad id', function(done) {
-    json('post', '/api/users/foobar')
+describe('Unexpected Usage', () => {
+  it('should not crash the server when posting a bad id', function() {
+    return json('post', '/api/users/foobar')
       .send({ })
-      .expect(404, done);
+      .expect(404);
   });
 });
